33ba2bf02dd79a2648fd1908d04b212520a3c62d
marshki <marshall@morethantomorrow.org>
2018-05-03 11:34:04 -0400

    unzip from absolute path

--- adobe_installer.sh
+++ adobe_installer.sh
@@ -14,7 +14,7 @@ LOCAL_WEB="128.122.112.23"
 
 # Is current UID 0? If not, exit.
 
-function root_check () {
+root_check () {
 
   if [ "$EUID" -ne "0" ] ; then
     printf "%s\n" "Error: root privileges are required to continue. Exiting." >&2
@@ -24,17 +24,17 @@ fi
 
 # Is there adequate disk space in "/Applications"? If not, exit.
 
-function check_disk_space () {
+check_disk_space () {
 
-  if [ $(df -Hl /Applications |awk 'FNR == 2 {print $4}' |sed 's/G//') -le 10 ]; then
-    printf "%s\n" "Error: Not enough free disk space. Exiting" >&2
-    exit 1
+if [ $(df -Hl /Applications |awk 'FNR == 2 {print $4}' |sed 's/G//') -le 10 ]; then
+  printf "%s\n" "Error: Not enough free disk space. Exiting" >&2
+  exit 1
 fi
 }
 
 # Is curl installed? If not, exit. (Curl ships with OS X, but let's check).
 
-function curl_check () {
+curl_check () {
 
   if ! [ -x "$(command -v curl 2>/dev/null)" ]; then
     printf "%s\n" "Error: curl is not installed. Exiting."  >&2
@@ -44,7 +44,7 @@ fi
 
 # Is CNS local web available? If not, exit.
 
-function ping_local_web () {
+ping_local_web () {
 
   printf "%s\n" "Pinging CNS local web..."
 
@@ -58,24 +58,25 @@ function ping_local_web () {
 
 # Download Adobe DC .zip
 
-function get_acrobat () {
+get_acrobat () {
 
   printf "%s\n" "Retrieving Adobe Acrobat insaller..."
 
-  curl --progress-bar --retry 3 --retry-delay 5 "$ADOBE_ACROBAT" --output acrobat.zip
+  curl --progress-bar --retry 3 --retry-delay 5 "$ADOBE_ACROBAT" --output /Applications/acrobat.zip
 }
 
-# Unzip .zip to Applications
+# Unzip acrobat.zip to Applications
 
-function unzip_acrobat () {
+unzip_acrobat () {
+
   printf "%s\n" "Unzipping to /Applications..."
 
-  unzip acrobat.zip -d /Applications
+  unzip /Applications/acrobat.zip -d /Applications
 }
 
 # Run installer
 
-function install_acrobat () {
+install_acrobat () {
 
   printf "%s\n" "Installing Acrobat..."
 
@@ -84,7 +85,7 @@ function install_acrobat () {
 
 # Check if Acrobat is installed
 
-function confirm_acrobat () {
+confirm_acrobat () {
 
   if open -Ra "Atom" &> /dev/null; then
     printf "%s\n" "Atom is installed. Woohoo!"
@@ -93,44 +94,28 @@ function confirm_acrobat () {
 fi
 }
 
+# Remove .zip file and installer
 
+remove_zip () {
 
-function confirm_acrobat () {
-  printf "%s\n" "Checking /Applications for Acrobat.."
+  printf "%s\n" "Removing Acrobat.zip and mac-acrobat-spr18."
 
-
-  if find /Applications -iname "Atom.app" &> /dev/null; then
-    printf "%s\n" "Atom is installed. Woohoo!"
-  else
-    printf "%s\n" "Error: Atom is not installed. Zoinks!" >&2
-    exit 1
-fi
-}
-
-  #find /Applications -iname "Adobe Acrobat.app"
-
-# Remove .zip file
-
-function remove_zip () {
-
-  printf "%s\n" "Removing Acrobat .zip"
-
-  rm -rf acrobat.zip
+  rm -rf acrobat.zip && rm -rf /Applications/mac-acrobatdc-spr18
 }
 
 # Download Adobe Illustrator zip
-#function get_illustrator () {
+# get_illustrator () {
 #  printf "%s\n" "Retrieving Adobe Acrobat insaller..."
 #  curl --progress-bar --retry 3 --retry-delay 5 "$ADOBE_ILLUSTRATOR" --output illustrator.zip
 #}
 
 # Download Adobe Photoshop zip
-#function get_photoshop () {
+# get_photoshop () {
 #  printf "%s\n" "Retrieving Adobe Acrobat insaller..."
 #  curl --progress-bar --retry 3 --retry-delay 5 "$ADOBE_PHOTOSHOP" --output photoshop.zip
 #}
 
-# Main function
+# Main 
 
 main () {
   root_check
