acfb7b278f8920a73337af306e8854e3421f3bbe
marshki <marshall@morethantomorrow.org>
2018-05-04 15:15:53 -0400

    fix spacing

--- adobe_installer.sh
+++ adobe_installer.sh
@@ -16,7 +16,6 @@ LOCAL_WEB="128.122.112.23"
 # Is current UID 0? If not, exit.
 
 root_check () {
-
   if [ "$EUID" -ne "0" ] ; then
     printf "%s\n" "Error: root privileges are required to continue. Exiting." >&2
     exit 1
@@ -26,17 +25,15 @@ fi
 # Is there adequate disk space in "/Applications"? If not, exit.
 
 check_disk_space () {
-
-if [ $(df -Hl /Applications |awk 'FNR == 2 {print $4}' |sed 's/G//') -le 10 ]; then
-  printf "%s\n" "Error: Not enough free disk space. Exiting" >&2
-  exit 1
+  if [ $(df -Hl /Applications |awk 'FNR == 2 {print $4}' |sed 's/G//') -le 10 ]; then
+    printf "%s\n" "Error: Not enough free disk space. Exiting" >&2
+    exit 1
 fi
 }
 
 # Is curl installed? If not, exit. (Curl ships with OS X, but let's check).
 
 curl_check () {
-
   if ! [ -x "$(command -v curl 2>/dev/null)" ]; then
     printf "%s\n" "Error: curl is not installed. Exiting."  >&2
     exit 1
@@ -46,7 +43,6 @@ fi
 # Is CNS local web available? If not, exit.
 
 ping_local_web () {
-
   printf "%s\n" "Pinging CNS local web..."
 
   if ping -c 1 "$LOCAL_WEB" &> /dev/null; then
@@ -62,7 +58,6 @@ ping_local_web () {
 # Download Acrobat .zip to /Applications.
 
 get_acrobat () {
-
   printf "%s\n" "Retrieving Acrobat insaller..."
 
   curl --progress-bar --retry 3 --retry-delay 5 "$ADOBE_ACROBAT" --output /Applications/acrobat.zip
@@ -71,7 +66,6 @@ get_acrobat () {
 # Unzip acrobat.zip to /Applications.
 
 unzip_acrobat () {
-
   printf "%s\n" "Unzipping Acrobat to /Applications..."
 
   unzip /Applications/acrobat.zip -d /Applications
@@ -80,7 +74,6 @@ unzip_acrobat () {
 # Run Acrobat installer.
 
 install_acrobat () {
-
   printf "%s\n" "Installing Acrobat..."
 
   installer -pkg /Applications/mac-acrobatdc-spr18/Build/mac-acrobatdc-spr18_Install.pkg -target /
@@ -89,7 +82,6 @@ install_acrobat () {
 # Remove Acrobat .zip file and installer.
 
 remove_acrobat_zip () {
-
   printf "%s\n" "Removing acrobat.zip and mac-acrobat-spr18..."
 
   rm -rv /Applications/{acrobat.zip,mac-acrobatdc-spr18}
@@ -100,7 +92,6 @@ remove_acrobat_zip () {
 # Download Illustrator .zip to /Applications
 
 get_illustrator () {
-
   printf "%s\n" "Retrieving Illustrator insaller..."
 
   curl --progress-bar --retry 3 --retry-delay 5 "$ADOBE_ILLUSTRATOR" --output /Applications/illustrator.zip
@@ -109,7 +100,6 @@ get_illustrator () {
 # Unzip Illustrator to /Applications
 
 unzip_illustrator () {
-
   printf "%s\n" "Unzipping Illustrator to /Applications..."
 
   unzip /Applications/illustrator.zip -d /Applications
@@ -118,7 +108,6 @@ unzip_illustrator () {
 # Run Illustrator installer.
 
 install_illustrator () {
-
   printf "%s\n" "Installing Illustrator..."
 
   installer -pkg /Applications/mac-illustrator-spr18/Build/mac-illustrator-spr18_Install.pkg -target /
@@ -127,7 +116,6 @@ install_illustrator () {
 # Remove Illustrator .zip file and installer.
 
 remove_illustrator_zip () {
-
   printf "%s\n" "Removing illustrator.zip and mac-illustrator-spr18."
 
   rm -rv /Applications/{illustrator.zip,mac-illustrator-spr18}
@@ -145,7 +133,6 @@ get_photoshop () {
 # Unzip Photoshop to /Applications
 
 unzip_photoshop () {
-
   printf "%s\n" "Unzipping Photoshop to /Applications..."
 
   unzip /Applications/photoshop.zip -d /Applications
@@ -154,7 +141,6 @@ unzip_photoshop () {
 # Run Illustrator installer.
 
 install_photoshop () {
-
   printf "%s\n" "Installing Photoshop..."
 
   installer -pkg /Applications/mac-photoshop-spr18/Build/mac-photoshop-spr18_Install.pkg -target /
@@ -163,7 +149,6 @@ install_photoshop () {
 # Remove Photoshop .zip file and installer.
 
 remove_photoshop_zip () {
-
   printf "%s\n" "Removing photoshop.zip and mac-photoshop-spr18."
 
   rm -rv /Applications/{photoshop.zip,mac-photoshop-spr18}
@@ -172,7 +157,6 @@ remove_photoshop_zip () {
 # Main
 
 sanity_checks () {
-
   root_check
   check_disk_space
   curl_check
@@ -180,7 +164,6 @@ sanity_checks () {
 }
 
 acrobat () {
-
   get_acrobat
   unzip_acrobat
   install_acrobat
@@ -188,7 +171,6 @@ acrobat () {
 }
 
 illustrator () {
-
   get_illustrator
   unzip_illustrator
   install_illustrator
@@ -196,7 +178,6 @@ illustrator () {
 }
 
 photoshop () {
-
   get_photoshop
   unzip_photoshop
   install_photoshop
