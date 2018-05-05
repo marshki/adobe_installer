ef826626e62f2718763cf3432e0a6acb1403490c
System Administrator <root@MACSTER.CNS.NYU.EDU>
2018-05-05 11:26:10 -0400

    function spacing fix

--- adobe_installer.sh
+++ adobe_installer.sh
@@ -15,7 +15,7 @@ LOCAL_WEB="128.122.112.23"
 
 # Is current UID 0? If not, exit.
 
-root_check () {
+root_check() {
   if [ "$EUID" -ne "0" ] ; then
     printf "%s\n" "Error: root privileges are required to continue. Exiting." >&2
     exit 1
@@ -24,7 +24,7 @@ fi
 
 # Is there adequate disk space in "/Applications"? If not, exit.
 
-check_disk_space () {
+check_disk_space() {
   if [ $(df -Hl /Applications |awk 'FNR == 2 {print $4}' |sed 's/G//') -le 10 ]; then
     printf "%s\n" "Error: Not enough free disk space. Exiting" >&2
     exit 1
@@ -33,7 +33,7 @@ fi
 
 # Is curl installed? If not, exit. (Curl ships with OS X, but let's check).
 
-curl_check () {
+curl_check() {
   if ! [ -x "$(command -v curl 2>/dev/null)" ]; then
     printf "%s\n" "Error: curl is not installed. Exiting."  >&2
     exit 1
@@ -42,7 +42,7 @@ fi
 
 # Is CNS local web available? If not, exit.
 
-ping_local_web () {
+ping_local_web() {
   printf "%s\n" "Pinging CNS local web..."
 
   if ping -c 1 "$LOCAL_WEB" &> /dev/null; then
@@ -55,7 +55,7 @@ ping_local_web () {
 
 #### Menu ####
 
-# Display pause prompt #
+# Display pause prompt 
 # Suspend processing of script; display message prompting user to press [Enter] key to continue
 # $1-> Message (optional)
 
@@ -83,7 +83,7 @@ function show_menu() {
 
 # Download Acrobat .zip to /Applications.
 
-get_acrobat () {
+get_acrobat() {
   printf "%s\n" "Retrieving Acrobat insaller..."
 
   curl --progress-bar --retry 3 --retry-delay 5 "$ADOBE_ACROBAT" --output /Applications/acrobat.zip
@@ -91,7 +91,7 @@ get_acrobat () {
 
 # Unzip acrobat.zip to /Applications.
 
-unzip_acrobat () {
+unzip_acrobat() {
   printf "%s\n" "Unzipping Acrobat to /Applications..."
 
   unzip /Applications/acrobat.zip -d /Applications
@@ -99,7 +99,7 @@ unzip_acrobat () {
 
 # Run Acrobat installer.
 
-install_acrobat () {
+install_acrobat() {
   printf "%s\n" "Installing Acrobat..."
 
   installer -pkg /Applications/mac-acrobatdc-spr18/Build/mac-acrobatdc-spr18_Install.pkg -target /
@@ -107,7 +107,7 @@ install_acrobat () {
 
 # Remove Acrobat .zip file and installer.
 
-remove_acrobat_zip () {
+remove_acrobat_zip() {
   printf "%s\n" "Removing acrobat.zip and mac-acrobat-spr18..."
 
   rm -rv /Applications/{acrobat.zip,mac-acrobatdc-spr18}
@@ -117,7 +117,7 @@ remove_acrobat_zip () {
 
 # Download Illustrator .zip to /Applications
 
-get_illustrator () {
+get_illustrator() {
   printf "%s\n" "Retrieving Illustrator insaller..."
 
   curl --progress-bar --retry 3 --retry-delay 5 "$ADOBE_ILLUSTRATOR" --output /Applications/illustrator.zip
@@ -125,7 +125,7 @@ get_illustrator () {
 
 # Unzip Illustrator to /Applications
 
-unzip_illustrator () {
+unzip_illustrator() {
   printf "%s\n" "Unzipping Illustrator to /Applications..."
 
   unzip /Applications/illustrator.zip -d /Applications
@@ -133,7 +133,7 @@ unzip_illustrator () {
 
 # Run Illustrator installer.
 
-install_illustrator () {
+install_illustrator() {
   printf "%s\n" "Installing Illustrator..."
 
   installer -pkg /Applications/mac-illustrator-spr18/Build/mac-illustrator-spr18_Install.pkg -target /
@@ -141,7 +141,7 @@ install_illustrator () {
 
 # Remove Illustrator .zip file and installer.
 
-remove_illustrator_zip () {
+remove_illustrator_zip() {
   printf "%s\n" "Removing illustrator.zip and mac-illustrator-spr18."
 
   rm -rv /Applications/{illustrator.zip,mac-illustrator-spr18}
@@ -149,7 +149,7 @@ remove_illustrator_zip () {
 
 # Download Photoshop .zip to /Applications
 
-get_photoshop () {
+get_photoshop() {
   printf "%s\n" "Retrieving Photoshop insaller..."
 
   curl --progress-bar --retry 3 --retry-delay 5 "$ADOBE_PHOTOSHOP" --output /Applications/photoshop.zip
@@ -157,7 +157,7 @@ get_photoshop () {
 
 # Unzip Photoshop to /Applications
 
-unzip_photoshop () {
+unzip_photoshop() {
   printf "%s\n" "Unzipping Photoshop to /Applications..."
 
   unzip /Applications/photoshop.zip -d /Applications
@@ -165,7 +165,7 @@ unzip_photoshop () {
 
 # Run Illustrator installer.
 
-install_photoshop () {
+install_photoshop() {
   printf "%s\n" "Installing Photoshop..."
 
   installer -pkg /Applications/mac-photoshop-spr18/Build/mac-photoshop-spr18_Install.pkg -target /
@@ -173,7 +173,7 @@ install_photoshop () {
 
 # Remove Photoshop .zip file and installer.
 
-remove_photoshop_zip () {
+remove_photoshop_zip() {
   printf "%s\n" "Removing photoshop.zip and mac-photoshop-spr18."
 
   rm -rv /Applications/{photoshop.zip,mac-photoshop-spr18}
@@ -203,28 +203,28 @@ trap '' SIGINT SIGQUIT SIGTSTP
 
 #### Main ####
 
-sanity_checks () {
+sanity_checks() {
   root_check
   check_disk_space
   curl_check
   ping_local_web
 }
 
-acrobat () {
+acrobat() {
   get_acrobat
   unzip_acrobat
   install_acrobat
   remove_acrobat_zip
 }
 
-illustrator () {
+illustrator() {
   get_illustrator
   unzip_illustrator
   install_illustrator
   remove_illustrator_zip
 }
 
-photoshop () {
+photoshop() {
   get_photoshop
   unzip_photoshop
   install_photoshop
@@ -235,6 +235,7 @@ sanity_checks
 
 while true
 do
+    clear
     show_menu
     read_input
 done
